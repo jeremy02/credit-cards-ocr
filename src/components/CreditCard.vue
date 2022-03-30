b0db8272d4f2347391131c22b5a60fc706962f7f
Jeremiah Thuku <jeremiah.thuku@ncbagroup.com>
2022-03-30 17:07:48 +0300

    working code to extract text

--- src/components/CreditCard.vue
+++ src/components/CreditCard.vue
@@ -36,53 +36,12 @@
                     <div class="text-xs-center" v-show="loader">
                         <v-progress-circular indeterminate v-bind:size="100" v-bind:width="3" class="teal--text"></v-progress-circular>
                     </div>
-                    <div v-show="result" class="text-md-center">
-                        <p>Extracted Text: <span class="blue--text">{{textAnnotation}}</span></p>
-                        <p>Full Text: <span class="red--text">{{fullTextAnnotation}}</span></p>
-                        <p class="black--text" v-for="(item, index) in fullTextAnnotationSplit" :key="index">
+                    <div v-show="textAnnotationsDesc || fullTextAnnotationsDesc" class="text-md-center">
+                        <p v-show="textAnnotationsDesc">Extracted Text: <span class="blue--text">{{ textAnnotationsDesc }}</span></p>
+                        <p v-show="fullTextAnnotationsDesc">Full Text: <span class="red--text">{{ fullTextAnnotationsDesc }}</span></p>
+                        <!-- <p class="black--text" v-for="(item, index) in fullTextAnnotationSplit" :key="index">
                             {{ index }} - {{ item }}
-                        </p>
-                    </div>
-                </v-flex>
-            
-            </v-layout>           
-       </div> 
-       
-       <hr/>
-
-       <div class="clearfix mt-4">
-            <v-layout row wrap>
-                <!--The Image-->
-                <v-flex xs12 sm12 md6 lg6 xl6>
-                    <v-card>   
-                        <img style="" width="100%" height="50%" :src="myImage" alt="Credit Card Image">
-                        <canvas id="canvas" width="600" height="480" style="display: none;"></canvas>
-                    </v-card>
-
-                    <br/>
-                    <br/>    
-                    <input @change="handleImage" class="custom-input" type="file" block>
-                    <br/>
-                    <br/>
-
-                    <v-btn @click.native="process" block class="red">
-                        <v-icon left>camera_alt</v-icon> Scan
-                    </v-btn>
-
-                </v-flex>
-
-                <!--The Results-->
-                <v-flex xs12 sm12 md6 lg6 xl6>
-                    <h2 class="orange--text text-xs-center">Result</h2>
-                    <div class="text-xs-center" v-show="loader">
-                        <v-progress-circular indeterminate v-bind:size="100" v-bind:width="3" class="teal--text"></v-progress-circular>
-                    </div>
-                    <div v-show="result" class="text-md-center">
-                        <p>Extracted Text: <span class="blue--text">{{textAnnotation}}</span></p>
-                        <p>Full Text: <span class="red--text">{{fullTextAnnotation}}</span></p>
-                        <p class="black--text" v-for="(item, index) in fullTextAnnotationSplit" :key="index">
-                            {{ index }} - {{ item }}
-                        </p>
+                        </p> -->
                     </div>
                 </v-flex>
             
@@ -105,121 +64,23 @@ export default {
         return{
             loader: false,
             result: false,
-            myImage: "",
-            apiKey: "AIzaSyAuX0eP9CBzbkfqrVtCC0gPL4BiUm_EUUo",  //google cloud api  Browser key 
-            textAnnotation: null,
-            fullTextAnnotation: null,
-            fullTextAnnotationSplit: [],
-            data: {               //type vision api Request
-                "requests": [{
-                    "features": [{
-                        "type": "FACE_DETECTION"
-                    }],
-                    "image": {
-                        "content": null
-                    }
-                }]
-            },
-            textData : { // OCR request
-                "requests":[
-                {
-                    "image":{
-                            "content":null,
-                            },
-                    "features": [
-                    {
-                        "type":"TEXT_DETECTION" // DOCUMENT_TEXT_DETECTION
-                    }
-                    ],
-                    "imageContext": {
-                        "languageHints": ["en"]
-                    }
-                }
-                ]
-            }   
         }
     },
     
-    computed: mapGetters({
-        usersList: 'usersList',
-        selectedImage: 'selectedImage',
-        selectedImageBase64: 'selectedImageBase64',
-        messageResult: 'messageResult',
-    }),
+    computed: {
+        ...mapGetters({
+            usersList: 'usersList',
+            selectedImage: 'selectedImage',
+            selectedImageBase64: 'selectedImageBase64',
+            messageResult: 'messageResult',
+            textAnnotationsDesc: 'textAnnotationsDesc',
+            fullTextAnnotationsDesc: 'fullTextAnnotationsDesc',
+        }),
+    },
 
     methods: {
         ...mapActions(["handleImageSelect", "detectTextFromImage"]),
-        handleImage: function(e) {
-            const selectedImage = e.target.files[0]; // get first file
-            this.createBase64Image(selectedImage);
-        },
-        createBase64Image: function(fileObject) {
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                this.myImage = e.target.result;
-                // this.setImageUri();
-            };
-            reader.readAsDataURL(fileObject);
-        },
-        process: function() {
-            const vm = this;
-            this.result = false;
-            this.loader = true;
 
-            const base64 = vm.myImage;
-
-            // Convert the image data to a Buffer and base64 encode it.
-            // const encodedImage = Buffer.from(vm.myImage).toString('base64');
-            
-            // const finalImage = base64.replace("data:image/png;base64,", "");
-
-            const finalImage = base64.substring(base64.indexOf("base64,") + 7);
-        
-            // Text OCR Detection
-            vm.textData.requests[0].image.content = finalImage; // encodedImage
-
-            axios.post(`https://vision.googleapis.com/v1/images:annotate?key=${vm.apiKey}`,
-                vm.textData
-                ).then(result => {
-                        const textDetections = result.data.responses[0];
-
-                        // we have all the descriptions from textAnnotations
-                        const annotationResult = textDetections.textAnnotations;
-
-                        /* for (let i = 1; i < annotationResult.length; i++) {
-                            console.log(i + ": " + annotationResult[i].description); 
-                        } */
-
-                        // console.log('Text:');
-                        // annotationResult.forEach(text => console.log(text))
-
-                        vm.textAnnotation = annotationResult[0].description ? annotationResult[0].description : "" ;
-
-                        // lets get the text from fullTextAnnotation
-                        vm.fullTextAnnotation = textDetections.fullTextAnnotation.text ? textDetections.fullTextAnnotation.text : '';
-
-                        vm.fullTextAnnotationSplit = vm.fullTextAnnotation.replace(/\r\n/g, "\r").replace(/\n/g, "\r").split(/\r/);
-
-                        vm.loader = false;
-                        vm.result = true;
-                        
-                }).catch(error => {
-                    console.log("detections.text Error::::"+error);
-                    vm.textAnnotation = null;
-                    vm.fullTextAnnotation = null;
-                })
-        
-            // context.drawImage(video, 0, 0, 640, 480);
-        },
-        confidenceInt(num){
-            const dig = num.toFixed(2);
-            if(dig == 1.0){
-                return 100;
-            }else{
-                const str = dig.toString();
-                return str.substring(2, 4);
-            }
-        },
     },
 }
 </script>
--- src/store/modules/credit-card-module.js
+++ src/store/modules/credit-card-module.js
@@ -13,41 +13,20 @@ const state = {
     messageResult: {
       success: false,
       message: null,
-    }
+    },
+    textAnnotationsDesc: null,
+    fullTextAnnotationsDesc: null
 };
 
 const getters = {
     selectedImage: state => state.selectedImage,
     selectedImageBase64: state => state.selectedImageBase64,
-    messageResult: state => state.messageResult
+    messageResult: state => state.messageResult,
+    textAnnotationsDesc: state => state.textAnnotationsDesc,
+    fullTextAnnotationsDesc: state => state.fullTextAnnotationsDesc
 };
 
 const actions = {
-    async detectTextFromImage({commit}){
-        // init the message to return
-        let msg = initialMessage;
-
-        // Check if we have an image to detect text
-        if(!state.selectedImageBase64) {
-            msg.message = "No file was found to detect text operation";
-            await commit("addMessage", msg)
-        }else{
-            // OCR/Google Vision Deetxt Text Request body 
-            let detextTextData = detectTextRequestBody.detectTextRequestBody   
-            
-            try {
-                const response = await detectTextApi.detectTextFromImage(detextTextData)
-            } catch (error) {
-                if(error.response.data.error.message) {
-                    msg.message = error.response.data.error.message
-                    await commit("addMessage", msg)
-                }else{
-                    msg.message = error.message
-                    await commit("addMessage", msg)
-                }
-            }
-        }
-    },
     async handleImageSelect({commit}, event){
         // reset the data and fields 
         commit("resetData")
@@ -76,7 +55,40 @@ const actions = {
           // add the image to store
           await commit("setSelectedImage", event.target.files[0])  // get first file
       }
-    }
+    },
+    async detectTextFromImage({commit}){
+        // init the message to return
+        let msg = initialMessage;
+
+        // Check if we have an image to detect text
+        if(!state.selectedImageBase64) {
+            msg.message = "No file was found to detect text operation";
+            await commit("addMessage", msg)
+        }else{
+            // OCR/Google Vision Deetxt Text Request body 
+            let detextTextData = detectTextRequestBody.detectTextRequestBody   
+            
+            try {
+                const base64Image = (state.selectedImageBase64).substring((state.selectedImageBase64).indexOf("base64,") + 7);
+            
+                // add the base64 image to the request body
+                detextTextData.requests[0].image.content = base64Image;
+
+                const response = await detectTextApi.detectTextFromImage(detextTextData)
+                
+                commit("detectTextResponse", response.data)
+            } catch (error) {
+                if(error.response.data.error.message) {
+                    msg.message = error.response.data.error.message
+                }else{
+                    msg.message = error.message
+                }
+                
+                // add the error message
+                await commit("addMessage", msg)
+            }
+        }
+    },
 };
 
 const mutations = { 
@@ -98,9 +110,29 @@ const mutations = {
         // reset the data
         state.selectedImage = null;
         state.selectedImageBase64 = null;
-        state.messageResult = initialMessage;
+        state.messageResult = initialMessage
+    },
+    detectTextResponse: function(state, result) {
+        const detectedTextResponse = result.responses[0];
+
+        // we have all the descriptions from textAnnotations
+        const annotationResult = detectedTextResponse.textAnnotations;
+
+        // annotationResult.forEach(text => console.log(text))
+
+        // this is the text annotation
+        state.textAnnotationsDesc = annotationResult[0].description ? annotationResult[0].description : null
+
+        // this is the full text annotation
+        state.fullTextAnnotationsDesc = detectedTextResponse.fullTextAnnotation.text ? detectedTextResponse.fullTextAnnotation.text : null
+
+        // state.fullTextAnnotationsDesc = vm.fullTextAnnotation.replace(/\r\n/g, "\r").replace(/\n/g, "\r").split(/\r/);
+
+        console.log(">>>>>>>>>>>>>1>>>>"+JSON.stringify(state.textAnnotationsDesc))
+        console.log(">>>>>>>>>>>>>2>>>>"+JSON.stringify(state.fullTextAnnotationsDesc))
 
     }
+    
 };
 
 export default {
