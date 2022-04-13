a30888730cefabd82d0aa09d0f3eaa1b3fe7d18a
Jeremiah Thuku <jeremiah.thuku@ncbagroup.com>
2022-04-13 14:08:35 +0300

    finished with the project and readme

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  GOOGLE_API_KEY: '"AIzaSyAuX0eP9CBzbkfqrVtCC0gPL4BiUm_EUUo"'
+  GOOGLE_API_KEY: '""'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,4 @@
 module.exports = {
   NODE_ENV: '"production"',
-  GOOGLE_API_KEY: '"AIzaSyAuX0eP9CBzbkfqrVtCC0gPL4BiUm_EUUo"'
+  GOOGLE_API_KEY: '""'
 }
